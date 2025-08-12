9ec1f80306bdd701e392b592712a56b716f81c71
Samuriot <92127962+Samuriot@users.noreply.github.com>
2025-08-12 10:30:59 -0700

    changed init script & ported dependencies to requirements.txt

--- init.sh
+++ init.sh
@@ -1,10 +1,5 @@
 source bot-env/bin/activate
 
 # dependencies
-pip install pdfplumber
-pip install discord
-pip install dotenv
-pip install -q -U google-enai
-pip install supabase
-pip install requests
+pip install -r requirements.txt
 
